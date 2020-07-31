3528acfa4173b3c09819bea9fedffba934533788
0918nobita <nobita.0918@gmail.com>
2020-07-31 14:10:54 +0900

    :sparkles: ZAM.Interp.Tests プロジェクトを追加

---
+++ src/ZAM.Interp.Tests/Program.fs
@@ -0,0 +1,20 @@
+﻿open Expecto
+
+[<Tests>]
+let tests =
+    test "simple test" {
+        let subject = "Hello, world"
+        Expect.equal subject "Hello, world" "The strings should equal"
+    }
+
+[<Tests>]
+let properties =
+    testList "FsCheck"
+        [ testProperty "Addition is commutative" <| fun a b -> a + b = b + a
+
+          testProperty "reverse list" <| fun (xs: int list) ->
+              let reversed = List.rev xs
+              List.rev reversed = xs ]
+
+[<EntryPoint>]
+let main argv = runTestsInAssembly defaultConfig argv
