56404c650dd46f7ba7062413096c9a33e6f4c947
0918nobita <nobita.0918@gmail.com>
2020-08-03 14:28:53 +0900

    :construction: 各種リテラル・2項演算・名前参照の型チェックを実装した

--- src/ZAM.Interp/Program.fs
+++ src/ZAM.Interp/Program.fs
@@ -1,11 +1,27 @@
 module Program
 
 module BResult = Base.Result
-open FSharp.Json
-open System.IO
+// open FSharp.Json
+// open System.IO
+open TypedExpr
 
 [<EntryPoint>]
 let main argv =
+    let typedExpr = TEBinApp(TEAdd, TEInt 3, TEInt 4)
+    printfn "TypedExpr: %O" typedExpr
+    let res = BResult.result {
+        let! (ty, untypedExpr) = typeCheck [] typedExpr
+        printfn "UntypedExpr: %O" untypedExpr
+        printfn "Type: %O" ty
+        return! Runtime.eval [] untypedExpr
+    }
+    match res with
+    | Ok v -> printfn "Result: %O" v
+    | Error msg ->
+        eprintfn "%s" msg
+        exit 1
+
+    (*
     if Array.isEmpty argv then
         eprintfn "ファイル名が指定されていません"
         exit 1
@@ -34,4 +50,5 @@ let main argv =
         eprintfn "%s" msg
         exit 1
 
+    *)
     0
--- src/ZAM.Interp/TypedExpr.fs
+++ src/ZAM.Interp/TypedExpr.fs
@@ -1,6 +1,9 @@
 module TypedExpr
 
+module BOption = Base.Option
+module BResult = Base.Result
 open Type
+open UntypedExpr
 
 type TEVarId = string
 
@@ -26,3 +29,55 @@ type TypedExpr =
     | TEMakeRef of TypedExpr
     | TEDeref of TypedExpr
     | TEMut of TypedExpr * TypedExpr
+
+let assertType (expected: Type) (actual: Type): Result<unit, string> =
+    if expected = actual
+        then Ok ()
+        else Error (sprintf "expected: %O, actual: %O" expected actual)
+
+let rec typeCheck (env: TypeEnv) (expr: TypedExpr): Result<(Type * UntypedExpr), string> =
+    match expr with
+    | TEUnit -> Ok (TUnit, UUnit)
+    | TEBool b -> Ok (TBool, UBool b)
+    | TEInt n -> Ok (TInt, UInt n)
+    | TEBinApp(op, lhs, rhs) ->
+        let mapError = Result.mapError (sprintf "(TypeError) BinApp:\n\t%O")
+        BResult.result {
+            let! (lhsType, lhs) = typeCheck env lhs
+            let! (rhsType, rhs) = typeCheck env rhs
+            match op with
+            | TEAdd ->
+                do! mapError (assertType TInt lhsType)
+                do! mapError (assertType TInt rhsType)
+                return (TInt, UBinApp (Add, lhs, rhs))
+            | TESub ->
+                do! mapError (assertType TInt lhsType)
+                do! mapError (assertType TInt rhsType)
+                return (TInt, UBinApp (Sub, lhs, rhs))
+            | TEMul ->
+                do! mapError (assertType TInt lhsType)
+                do! mapError (assertType TInt rhsType)
+                return (TInt, UBinApp (Mul, lhs, rhs))
+            | TEEq ->
+                do! mapError (assertType TInt lhsType)
+                do! mapError (assertType TInt rhsType)
+                return (TBool, UBinApp (Eq, lhs, rhs))
+            | TELt ->
+                do! mapError (assertType TInt lhsType)
+                do! mapError (assertType TInt rhsType)
+                return (TBool, UBinApp (Lt, lhs, rhs))
+            | TELe ->
+                do! mapError (assertType TInt lhsType)
+                do! mapError (assertType TInt rhsType)
+                return (TBool, UBinApp (Le, lhs, rhs))
+        }
+    | TEVar x ->
+        BResult.result {
+            let! ty =
+                List.tryFind (fst >> (=) x) env
+                |> Option.map snd
+                |> BOption.toResult
+                |> Result.mapError (fun () -> sprintf "(TypeError) Unbound identifier: %s" x)
+            return (ty, UVar x)
+        }
+    | _ -> Error "unimplemented"
