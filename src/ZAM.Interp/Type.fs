dc944355e8648cd82a3c3afc1dc56e535cdbd3bf
0918nobita <nobita.0918@gmail.com>
2020-08-03 11:14:00 +0900

    :construction: TypeVarId, Type, TypeEnv 型を定義した

--- src/ZAM.Interp/Type.fs
+++ src/ZAM.Interp/Type.fs
@@ -1,1 +1,13 @@
 module Type
+
+type TypeVarId = string
+
+type Type =
+    | TUnit
+    | TInt
+    | TBool
+    | TVar of TypeVarId
+    | TFun of Type * Type
+    | Ref of Type
+
+type TypeEnv = List<TypeVarId * Type>
