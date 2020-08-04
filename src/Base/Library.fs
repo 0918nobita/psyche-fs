9f11e3d01bb3bacc6f4f88eaa34fa9198bf5ee5c
0918nobita <nobita.0918@gmail.com>
2020-08-04 11:44:31 +0900

    :sparkles: Base に Nel 型とその操作関数を追加した

--- src/Base/Library.fs
+++ src/Base/Library.fs
@@ -1,7 +1,41 @@
 namespace Base
 
+open System.Collections
+open System.Collections.Generic
+
 module Map =
     let keys (m: Map<_, _>) = Map.fold (fun keys key _ -> key :: keys) [] m
+
+module Nel =
+    type Nel<'a> =
+        private
+        | Nel of 'a * list<'a>
+
+        interface IReadOnlyCollection<'a> with
+
+            member this.Count =
+                match this with
+                | Nel(_, tail) -> 1 + List.length tail
+
+            member this.GetEnumerator() =
+                match this with
+                | Nel(head, tail) -> (head :: tail :> _ seq).GetEnumerator()
+
+            member this.GetEnumerator() =
+                match this with
+                | Nel(head, tail) ->
+                    (head :: tail :> _ seq).GetEnumerator() :> IEnumerator
+
+    module ActivePattern =
+        let (|Nel|) (input: Nel<'a>) =
+            match input with
+            | Nel(head, tail) -> (head, tail)
+
+    let singleton v = Nel(v, [])
+    let create head tail = Nel(head, tail)
+    let head (Nel(head, _)) = head
+    let tail (Nel(_, tail)) = tail
+    let length (Nel(_, tail)) = 1 + List.length tail
 
 module Option =
     type OptionBuilder() =
