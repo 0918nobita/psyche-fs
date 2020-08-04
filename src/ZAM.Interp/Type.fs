469de3c99dc8b18fcf40170a4d3b262e7fc1d783
0918nobita <nobita.0918@gmail.com>
2020-08-04 15:14:11 +0900

    :sparkles: Type.ToString メソッドをオーバーライド

--- src/ZAM.Interp/Type.fs
+++ src/ZAM.Interp/Type.fs
@@ -9,4 +9,12 @@ type Type =
     | TFun of Type * Type
     | TRef of Type
 
+    override this.ToString() =
+        match this with
+        | TUnit -> "Unit"
+        | TInt -> "Int"
+        | TBool -> "Bool"
+        | TFun(arg, ret) -> sprintf "(-> %O %O)" arg ret
+        | TRef ty -> sprintf "(Ref %O)" ty
+
 type TypeEnv = List<TypeVarId * Type>
