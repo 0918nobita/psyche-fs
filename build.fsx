d7687e9f2087366f74c5673415a39a3099a30897
0918nobita <nobita.0918@gmail.com>
2020-08-10 17:42:41 +0900

    :wrench: Publish ターゲットを追加

--- build.fsx
+++ build.fsx
@@ -30,4 +30,13 @@ Target.create "Test" (fun _ -> dotnet "run" "--project src/Psyche.Base.Test")
 
 "Clean" ==> "Test"
 
+Target.create "Publish" (fun _ ->
+    match Environment.isWindows, Environment.isMacOS, Environment.isLinux with
+    | true, false, false -> dotnet "publish" "-c Release --self-contained --runtime win-x64 --nologo"
+    | false, true, false -> dotnet "publish" "-c Release --self-contained --runtime osx-x64 --nologo"
+    | false, false, true -> dotnet "publish" "-c Release --self-contained --runtime linux-x64 --nologo"
+    | _, _, _ -> failwith "Publishing project in this platform is not supported")
+
+"Clean" ==> "Publish"
+
 Target.runOrDefault "Debug"
