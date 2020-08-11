35da7eed44d1cb9edab18c33e90afafb38016911
0918nobita <nobita.0918@gmail.com>
2020-08-11 12:22:31 +0900

    :sparkles: State 型/モジュールを追加

--- src/Psyche.Base/Library.fs
+++ src/Psyche.Base/Library.fs
@@ -66,3 +66,20 @@ module Base =
                 result {
                     let! s = state
                     return! folder s elem }) (Ok initialState) list
+
+    type State<'s, 'a> = State of ('s -> ('a * 's))
+
+    module State =
+        let runState ((State f): State<'s, 'a>) (init: 's) =
+            f init
+
+        type StateBuilder() =
+            member _.Return(x) = State(fun s -> (x, s))
+            member _.ReturnFrom(m: State<_, _>) = m
+            member _.Bind((State x), f) =
+                State
+                <| fun s ->
+                    let (v, s) = x s
+                    runState (f v) s
+
+        let state = StateBuilder()
