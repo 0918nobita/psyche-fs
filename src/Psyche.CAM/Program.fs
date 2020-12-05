23a7bd658bb7142788e2e107d51f65fb7beec850
0918nobita <nobita.0918@gmail.com>
2020-12-05 18:00:39 +0900

    :sparkles: CAM を実装

--- src/Psyche.CAM/Program.fs
+++ src/Psyche.CAM/Program.fs
@@ -1,6 +1,5 @@
 type CamInstr =
     | CAMLdi of int
-    | CAMLdf of float
     | CAMLdb of bool
     | CAMAccess of int
     | CAMClosure of CamCode
@@ -9,15 +8,19 @@ type CamInstr =
     | CAMLet
     | CAMEndLet
     | CAMTest of CamCode * CamCode
-    | CAMAddI
-    | CAMAddF
+    | CAMAdd
+    | CAMSub
+    | CAMMul
+    | CAMDiv
+    | CAMMod
     | CAMEq
+    | CAMLt
+    | CAMLe
 
 and CamCode = CamInstr list
 
 type CamValue =
     | CAMIntVal of int
-    | CAMFloatVal of float
     | CAMBoolVal of bool
     | CAMClosureVal of CamCode * CamEnv
 
@@ -29,37 +32,93 @@ let rec run (code: CamCode) (env: CamEnv) (stack: CamStack) : (CamValue * CamEnv
     match code with
     | [] -> (stack.[0], env, stack)
     | (CAMLdi i)::tail -> run tail env ((CAMIntVal i)::stack)
-    | (CAMLdf f)::tail -> run tail env ((CAMFloatVal f)::stack)
     | (CAMLdb b)::tail -> run tail env ((CAMBoolVal b)::stack)
     | (CAMAccess i)::tail -> run tail env (env.[i]::stack)
+    | CAMApply::tail ->
+        let closure = stack.[0]
+        let argument = stack.[1]
+        match closure with
+        | CAMClosureVal (closureCode, closureEnv) ->
+            run closureCode (argument::closure::closureEnv) (CAMClosureVal (tail, env)::stack.[2..])
+        | _ -> failwith "Invalid operation"
+    | CAMReturn::_ ->
+        let ret = stack.[0]
+        let cont = stack.[1]
+        match cont with
+        | CAMClosureVal (closureCode, closureEnv) ->
+            run closureCode closureEnv (ret::stack.[2..])
+        | _ -> failwith "Invalid operation"
     | (CAMClosure code)::tail ->
         run tail env ((CAMClosureVal (code, env))::stack)
     | CAMLet::tail ->
         run tail (stack.[0]::env) (stack.[1..])
     | CAMEndLet::tail ->
         run tail (env.[1..]) stack
-    | CAMAddI::tail ->
+    | (CAMTest (c1, c2))::tail ->
+        match stack.[0] with
+        | CAMBoolVal true ->
+            run (c1 @ tail) env stack.[1..]
+        | CAMBoolVal false ->
+            run (c2 @ tail) env stack.[1..]
+        | _ -> failwith "Invalid operation"
+    | CAMAdd::tail ->
         match (stack.[0], stack.[1]) with
         | (CAMIntVal rhs, CAMIntVal lhs) ->
             run tail env ((lhs + rhs |> CAMIntVal)::stack.[2..])
         | _ -> failwith "Invalid operation"
-    | CAMAddF::tail ->
+    | CAMSub::tail ->
         match (stack.[0], stack.[1]) with
-        | (CAMFloatVal rhs, CAMFloatVal lhs) ->
-            run tail env ((lhs + rhs |> CAMFloatVal)::stack.[2..])
+        | (CAMIntVal rhs, CAMIntVal lhs) ->
+            run tail env ((lhs - rhs |> CAMIntVal)::stack.[2..])
         | _ -> failwith "Invalid operation"
-    | _ -> failwith "Not implemented"
+    | CAMMul::tail ->
+        match (stack.[0], stack.[1]) with
+        | (CAMIntVal rhs, CAMIntVal lhs) ->
+            run tail env ((lhs * rhs |> CAMIntVal)::stack.[2..])
+        | _ -> failwith "Invalid operation"
+    | CAMDiv::tail ->
+        match (stack.[0], stack.[1]) with
+        | (CAMIntVal rhs, CAMIntVal lhs) ->
+            run tail env ((lhs / rhs |> CAMIntVal)::stack.[2..])
+        | _ -> failwith "Invalid operation"
+    | CAMMod::tail ->
+        match (stack.[0], stack.[1]) with
+        | (CAMIntVal rhs, CAMIntVal lhs) ->
+            run tail env ((lhs % rhs |> CAMIntVal)::stack.[2..])
+        | _ -> failwith "Invalid operation"
+    | CAMEq::tail ->
+        match (stack.[0], stack.[1]) with
+        | (CAMIntVal rhs, CAMIntVal lhs) ->
+            run tail env ((lhs = rhs |> CAMBoolVal)::stack.[2..])
+        | _ -> failwith "Invalid operation"
+    | CAMLt::tail ->
+        match (stack.[0], stack.[1]) with
+        | (CAMIntVal rhs, CAMIntVal lhs) ->
+            run tail env ((lhs < rhs |> CAMBoolVal)::stack.[2..])
+        | _ -> failwith "Invalid operation"
+    | CAMLe::tail ->
+        match (stack.[0], stack.[1]) with
+        | (CAMIntVal rhs, CAMIntVal lhs) ->
+            run tail env ((lhs <= rhs |> CAMBoolVal)::stack.[2..])
+        | _ -> failwith "Invalid operation"
 
 [<EntryPoint>]
 let main argv =
     let code =
-        [ CAMLdi 2
+        [ CAMClosure
+            [ CAMLdi 1
+            ; CAMAccess 0
+            ; CAMEq
+            ; CAMTest
+                ( [ CAMLdi 1 ]
+                , [ CAMLdi (-1); CAMAccess 0; CAMAdd; CAMAccess 1; CAMApply; CAMAccess 0; CAMAdd ]
+                )
+            ; CAMReturn
+            ]
         ; CAMLet
+        ; CAMLdi 10
         ; CAMAccess 0
-        ; CAMLdi 4
-        ; CAMLdi 1
-        ; CAMAddI
-        ; CAMAddI
+        ; CAMApply
         ; CAMEndLet
         ]
     let env = []
