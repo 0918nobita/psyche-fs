7e2c0be2958c7d0ff1dd58ade61c5f5638636ac0
0918nobita <nobita.0918@gmail.com>
2020-12-06 14:15:05 +0900

    :construction: バイトコードの形式を変更

--- src/Psyche.Compiler/Library.fs
+++ src/Psyche.Compiler/Library.fs
@@ -4,5 +4,10 @@ open System.IO
 
 let compile () =
     use writer = new BinaryWriter(File.Open("out.psyb", FileMode.Create))
-    writer.Write("PSYCHE"B) // MAGIC
-    writer.Write(1u) // VERSION
+    writer.Write("PSYC"B) // MAGIC
+    writer.Write(1u)      // VERSION
+    writer.Write(1uy)     // ldi
+    writer.Write(3)       //   3
+    writer.Write(1uy)     // ldi
+    writer.Write(4)       //   4
+    writer.Write(10uy)    // add
